966cd9814ef14de52006e47b072ab0a35042b836
Bhushan-1509 <bhushanmahajan09153@gmail.com>
2022-10-05 17:50:01 +0530

    Minor Changes 2

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template("index.html")
 
 @app.route('/predict')
 def predict():
-    return app.send_static_file('predict.html')
+    return render_template("predict.html")
 
 
 if __name__ == '__main__':
